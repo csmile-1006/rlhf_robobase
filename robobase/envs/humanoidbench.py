1cad837ada6cdebd8674fad7759f85020cb66c0b
csmile-1006 <doli1573@kaist.ac.kr>
2024-12-18 04:01:31 +0000

    misc

--- robobase/envs/humanoidbench.py
+++ robobase/envs/humanoidbench.py
@@ -203,10 +203,8 @@ class HumanoidBenchEnvFactory(EnvFactory):
         return env
 
     def make_train_env(self, cfg: DictConfig) -> gym.vector.VectorEnv:
-        # vec_env_class = gym.vector.AsyncVectorEnv
-        # kwargs = dict(context=None)
-        vec_env_class = gym.vector.SyncVectorEnv
-        kwargs = dict()
+        vec_env_class = gym.vector.AsyncVectorEnv
+        kwargs = dict(context="spawn")
         return vec_env_class(
             [
                 lambda: self._wrap_env(
