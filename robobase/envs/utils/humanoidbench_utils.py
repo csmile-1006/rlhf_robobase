31fb35dbb4ccf62ba944c95b21fa408896c4d00a
csmile-1006 <changyeon.kim@kaist.ac.kr>
2025-01-03 16:54:47 -0600

    add GR-1 walk

--- robobase/envs/utils/humanoidbench_utils.py
+++ robobase/envs/utils/humanoidbench_utils.py
@@ -1,5 +1,6 @@
 # ruff: noqa
 TASK_DESCRIPTION = {
-    "h1-walk-v0": "The humanoid robot must walk like a human without falling down.",
-    "g1-walk-v0": "The humanoid robot must walk like a human without falling down.",
+    "h1-walk-v0": "Make H1 humanoid robot walk at 1m/s velocity. The walking behavior must be like that of a human without falling down.",
+    "g1-walk-v0": "Make G1 humanoid robot walk at 1m/s velocity. The walking behavior must be like that of a human without falling down.",
+    "gr1-walk-v0": "Make GR1 humanoid robot walk at 1m/s velocity. The walking behavior must be like that of a human without falling down.",
 }
