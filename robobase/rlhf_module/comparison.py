e7bb621b237a1b896ae9e806305da3f118c4f690
csmile-1006 <changyeon.kim@kaist.ac.kr>
2025-01-09 18:37:03 -0600

    change details

--- robobase/rlhf_module/comparison.py
+++ robobase/rlhf_module/comparison.py
@@ -99,7 +99,7 @@ class MajorColumnComparisonFn(ComparisonFn):
         # Sort indices by major_column_returns
         sorted_indices = np.argsort(major_column_returns)
         # remove bottom 20% with particulary smaller major_column_returns
-        sorted_indices = sorted_indices[: int(len(sorted_indices) * 0.8)]
+        sorted_indices = sorted_indices[: int(len(sorted_indices) * 0.7)]
 
         # make pairs with similar major_column_returns
         self.indices = []
