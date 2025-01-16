2c5e0bdd9165dbdf432e9080db58d9bf73a54e00
csmile-1006 <doli1573@kaist.ac.kr>
2025-01-16 17:05:29 +0000

    add running

--- robobase/envs/utils/humanoidbench_utils.py
+++ robobase/envs/utils/humanoidbench_utils.py
@@ -1,6 +1,7 @@
 # ruff: noqa
 TASK_DESCRIPTION = {
     "h1-walk-v0": "The H1 humanoid robot should walk forward at 1m/s speed. The walking motion should be stable, energy-efficient, and closely resemble natural human walking.",
+    "h1-run-v0": "The H1 humanoid robot should run forward at 5m/s speed. The running motion should be stable, energy-efficient, and closely resemble natural human running.",
     "h1hand-walk-v0": "The H1 humanoid robot should walk forward at 1m/s speed without falling. The walking motion should be stable and closely resemble natural human walking.",
     "g1-walk-v0": "The G1 humanoid robot should walk forward at 1m/s speed. The walking motion should be stable, energy-efficient, and closely resemble natural human walking.",
     "gr1-walk-v0": "The GR-1 humanoid robot should walk forward at 1m/s speed without falling. The walking motion should be stable and closely resemble natural human walking.",
